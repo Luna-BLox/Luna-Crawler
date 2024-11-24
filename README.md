963bf88f67b3a416d81438324671dfc2a2bd06ec
gwashark <150717581+gwashark@users.noreply.github.com>
2024-11-24 13:11:08 -0500

    First Crawler V Init

---
+++ README.md
@@ -0,0 +1,170 @@
+
+# Lunablox Crawler
+LunaBlox Crawler is a web crawler for Roblox. It is built using the [Crawlee](https://github.com/apify/crawlee) framework in Node CommonJS (CJS).
+
+## Usage
+All you need to do is run these commands:
+
+```shell
+git clone https://github.com/Luna-BLox/Luna-Crawler.git
+cd Luna-Crawler
+npm run start
+```
+
+And then you're done 🎉
+## Support
+| Device | Supported |
+|--|--|
+| Windows |✅|
+| Mac OS | ✅❔|
+| Linux | ✅|
+
+## Supported Scrapes
+
+- [x] Charts (Discovery)
+- [x] [Games](#game-example)
+- [x] [Communities](#community-example) (Groups)
+- [x] [Badges](#badge-example)
+- [x] [Users](#user-example)
+
+> [!CAUTION]
+> These schemas aren't final and can be changed at any time!
+
+### Game Example
+```json
+{
+	"type": "game",
+	"title": "Example Game",
+	"description": "Example Game Description",
+	"creator": "Community or Roblox User",
+	"creatorUrl": "Roblox User or Community Url",
+	"likes": 12345678,
+	"dislikes": 12345678,
+	"active": "1,234",
+	"favorites": "1,234",
+	"visits": "99.9K+",
+	"voicechat": "Not Supported",
+	"camera": "Not Supported",
+	"created": "1/1/2024",
+	"updated": "1/1/2024",
+	"serversize": 12345678,
+	"genre": "N/A",
+	"subgenre": "N/A"
+}
+```
+- [x] Name
+- [x] Author
+- [ ] Maturity Level
+- [ ] Thumbnails 
+- [x] Likes
+- [x] Dislikes
+- [x] Description
+- [x] Active
+- [x] Favorites
+- [x] Visits
+- [x] Voice Chat Support
+- [x] Camera Support
+- [x] Created
+- [x] Updated
+- [x] Server Size
+- [x] Genre & Subgenre
+- [ ] Social Links
+- [ ] Badges
+- [ ] Events
+- [ ] Store (Subscription and Gamepasses)
+
+### Community Example
+```json
+{
+	"type": "community",
+	"url": "https://www.roblox.com/communities/12345678/Example#!/about",
+	"name": "Example Community",
+	"creator": "RobloxUser",
+	"creatorUrl": "https://www.roblox.com/users/1234567890/profile",
+	"members": 1234567890,
+	"description": "Example Community",
+	"roles": [
+		{
+			"name": "Member",
+			"assigned": "1k+"
+		},
+		{
+			"name": "Owner",
+			"assigned": "1"
+		},
+		...
+	],
+	"games": [
+		{
+		"name": "Example Game",
+			"url": "https://www.roblox.com/games/refer?PlaceId=1234567890"
+		},
+		{
+		"name": "Another Example Game",
+			"url": "https://www.roblox.com/games/refer?PlaceId=1234567890"
+		},
+		...
+	]
+}
+```
+
+- [x] Name
+- [x] Creator
+- [x] Member count
+- [x] Description
+- [x] Roles
+- [x] Games
+- [ ] Social Links
+- [ ] Events
+- [ ] Store
+- [ ] Affiliates
+
+### Badge Example
+```json
+{
+	"type": "badge",
+	"url": "https://www.roblox.com/badges/1234567890/Example-Badge",
+	"name": "Example Badge",
+	"creator": "Roblox User",
+	"game": "Example Game",
+	"gameUrl": "https://www.roblox.com/games/refer?PlaceId=1234567890"
+}
+```
+
+- [x] Name
+- [ ] Author (50% - Missing Author URL)
+- [x] Updated
+- [x] Description
+- [x] Game
+
+### User Example
+```json
+{
+	"type": "user",
+	"url": "https://www.roblox.com/users/1234567890/profile/",
+	"pfp": "https://tr.rbxcdn.com/30DAY-AvatarHeadshot...",
+	"displayName": "Roblox User",
+	"username": "@robloxuser",
+	"friends": 123,
+	"followers": 1234567,
+	"following": 1234567,
+	"about": "Example About"
+}
+```
+- [x] Display name
+- [x] username
+- [x] Friend Count
+- [x] Follower Count
+- [x] Following Count
+- [x] About/Description
+- [ ] Creations
+- [ ] Currently Wearing
+- [ ] Friends
+- [ ] Communities
+- [ ] Favorites
+- [ ] Roblox Badges
+- [ ] Badges
+- [ ] Statistics
+
+> [!NOTE] 
+> If you want to help out, please don't hesitate to [clone the repository](#usage) and create a [Pull Request](https://github.com/Luna-BLox/Luna-Crawler/pulls).
---
+++ src/main.js
@@ -0,0 +1,135 @@
+import { PlaywrightCrawler, Dataset } from "crawlee";
+
+const crawler = new PlaywrightCrawler({
+  async requestHandler({ page, enqueueLinks, log }) {
+    await page.waitForLoadState("networkidle");
+    if (page.url().includes("/charts/")) {
+      const gameLinks = await page.$$eval(".game-card-link", (elements) =>
+        elements.map((el) => el.href)
+      );
+      log.info(`Gathered ${gameLinks.length} game links.`);
+      for (const link of gameLinks) {
+        await crawler.addRequests([link]);
+      }
+    } else if (page.url().includes("/games/")) {
+      const gameInfo = await page.evaluate(() => {
+        return {
+          type: "game",
+          title: document
+            .querySelector(".game-title-container h1")
+            ?.textContent?.trim(),
+          description: document
+            .querySelector(".game-description")
+            ?.textContent?.trim(),
+          creator: document
+            .querySelector(".game-creator a")
+            ?.textContent?.trim(),
+          creatorUrl: document.querySelector(".game-creator a")?.href,
+          likes: Number(document.querySelector("#vote-up-text")?.title?.trim()),
+          dislikes: Number(
+            document.querySelector("#vote-down-text")?.title?.trim()
+          ),
+          active: document
+            .querySelectorAll(".game-stat")[0]
+            ?.childNodes[1]?.textContent?.trim(),
+          favorites: document
+            .querySelectorAll(".game-stat")[1]
+            ?.childNodes[1]?.textContent?.trim(),
+          visits: document
+            .querySelectorAll(".game-stat")[2]
+            ?.childNodes[1]?.textContent?.trim(),
+          voicechat: document
+            .querySelectorAll(".game-stat")[3]
+            ?.childNodes[1]?.textContent?.trim(),
+          camera: document
+            .querySelectorAll(".game-stat")[4]
+            ?.childNodes[1]?.textContent?.trim(),
+          created: document
+            .querySelectorAll(".game-stat")[5]
+            ?.childNodes[1]?.textContent?.trim(),
+          updated: document
+            .querySelectorAll(".game-stat")[6]
+            ?.childNodes[1]?.textContent?.trim(),
+          serversize: Number(document
+            .querySelectorAll(".game-stat")[7]
+            ?.childNodes[1]?.textContent?.trim()),
+          genre: document
+            .querySelectorAll(".game-stat")[8]
+            ?.childNodes[1]?.textContent?.trim(),
+          subgenre: document
+            .querySelectorAll(".game-stat")[9]
+            ?.childNodes[1]?.textContent?.trim() || "N/A",
+        };
+      });
+      await Dataset.pushData(gameInfo);
+      log.info(`Scraped game: ${gameInfo.title}`);
+    } else if (page.url().includes("/users/")) {
+      const userInfo = await page.evaluate(() => {
+        return {
+            type: "user",
+            url: window.location.href,
+            pfp: document.querySelector(".avatar-card-image").children[0]?.src,
+            displayName: document.querySelector(".profile-name")?.textContent?.trim(),
+            username: document.querySelector(".profile-display-name")?.textContent?.trim(),
+            friends: Number(document.querySelector(".details-info").children[0]?.children[1]?.children[0]?.title?.trim()),
+            followers: Number(document.querySelector(".details-info").children[1]?.children[1]?.children[0]?.title?.trim()),
+            following: Number(document.querySelector(".details-info").children[2]?.children[1]?.children[0]?.title?.trim()),
+            about: document.querySelector(".profile-about-content-text")?.textContent?.trim(),
+        }
+      })
+      await Dataset.pushData(userInfo);
+      log.info(`Scraped user: ${userInfo.username}`);
+    } else if (page.url().includes("/communities/")) {
+      const communityInfo = await page.evaluate(() => {
+        const games = [];
+        const roles = [];
+
+        document.querySelectorAll("group-games-item").forEach((game) => {
+          games.push({
+            name: game.querySelector(".game-card-name")?.textContent?.trim(),
+            url: game.childNodes[0]?.childNodes[1]?.href,
+          });
+        });
+
+        document.querySelectorAll("ul")[9].children.forEach((role) => {
+            roles.push({
+                name: role.children[0]?.children[0]?.textContent,
+                assigned: role.children[0]?.children[1]?.textContent || "0",
+            })
+        });
+        return {
+          type: "community",
+          url: window.location.href,
+          name: document.querySelectorAll(".group-name")[1]?.textContent?.trim(),
+          creator: document.querySelectorAll(".group-owner")[1].childNodes[4]?.textContent?.trim(),
+          creatorUrl:
+          document.querySelectorAll(".group-owner")[1].childNodes[4]?.href,
+          members: Number(document.querySelector(".group-members").childNodes[1]?.title?.trim()),
+          description: document
+            .querySelector(".group-description-content-text")
+            ?.textContent?.trim(),
+          roles,
+          games,
+        };
+      });
+      await Dataset.pushData(communityInfo);
+      log.info(`Scraped community (Formerly Groups): ${communityInfo.name}`);
+    } else if (page.url().includes("/badges/")) {
+      const badgeInfo = await page.evaluate(() => {
+        return {
+            type: "badge",
+            url: window.location.href,
+            name: document.querySelector(".item-name-container").children[0]?.textContent?.trim(),
+            creator: document.querySelector(".item-name-container")?.children[2]?.children[0]?.textContent?.replace("By ", "")?.trim(),
+            game: document.querySelector(".asset-info")?.children[0]?.textContent?.replace("Earn this Badge in: ", "")?.trim(),
+            gameUrl: document.querySelector(".asset-info").children[0]?.children[0]?.href,
+        }
+      })
+      await Dataset.pushData(badgeInfo);
+      log.info(`Scraped badge: ${badgeInfo.name}`);
+    }
+  },
+  headless: true, // Enable headless mode to run the crawler in the background
+});
+
+await crawler.run([]);
